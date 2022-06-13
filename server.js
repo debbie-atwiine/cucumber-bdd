481de0992ff8a59694fc344acc7434701ea15984
debbie_atwiine <deborahatwiine75@gmail.com>
2022-06-13 16:08:07 +0100

    initial changes

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.status(200).send("System is up and running");
+});
+
+const port = process.env.PORT || 5050 
+server.listen(port, () => {
+    console.log(`Server is listening at port ${port}`);
+});
