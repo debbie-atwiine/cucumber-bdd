48e59f466a840986bc4a1cdc12708d573dd63c80
Deborah Atwiine <deborahatwiine75@gmail.com>
2022-06-14 09:33:01 +0300

    using the timeouts code

--- features/steps-definition/steps.js
+++ features/steps-definition/steps.js
@@ -1,10 +1,10 @@
 const {When, Given, Then, wrapPromiseWithTimeout} = require('@cucumber/cucumber');
 const {expect} = require('chai');
 
-// Given('the operation completes within unknown minutes', {timeout: -1}, function(minutes) {
-//   const milliseconds = (minutes + 1) * 60 * 1000
-//   return wrapPromiseWithTimeout(this.verifyOperationComplete(), milliseconds);
-// });
+Given('the operation completes within unknown minutes', {timeout: -1}, function(minutes) {
+  const milliseconds = (minutes + 1) * 60 * 1000
+  return wrapPromiseWithTimeout(this.verifyOperationComplete(), milliseconds);
+});
 
 let pen = 0;
 
